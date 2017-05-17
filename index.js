4d5611c5ec934573811774a6abf7c8bba18eac79
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2017-05-17 17:03:56 -0400

    move code around abit

--- index.js
+++ index.js
@@ -1,127 +1,48 @@
-var Delaunator = require('delaunator');
-var Polygon = require('./lib/polygon');
 var now = require('performance-now');
 
+var makePolys = require('./lib/make-polys');
+var updatePolys = require('./lib/update-polys');
+
 module.exports = function(img, size) {
-  var data = [];
+  // SETUP THE POINTS AND POLYGONS
   var points = [
     [0,0],
     [img.shape[0]-1, 0],
     [img.shape[0]-1, img.shape[1]-1],
     [0, img.shape[1]-1]
   ];
-  for (var x=0; x<img.shape[0]; x++) {
-    for (var y=0; y<img.shape[1]; y++) {
-      var pix = {
-        r: img.get(x, y, 0),
-        g: img.get(x, y, 1),
-        b: img.get(x, y, 2),
-        x: x,
-        y: y
-      };
-      data.push(pix);
-    }
-  };
-
-  var numTris = 2;
-  var polys = makePolys(points, data);
-  polys.sort(Polygon.sortFn);
+  var polys = makePolys(points, img);
+  
+  // ADD POINTS UNTIL WE HAVE THE RIGHT NUMBER OF POLYGONS
+  var numTris = polys.length;
   while (numTris < size) {
     var start = now();
-    polys = updatePolys(polys, points);
-    var start = now();
-    var outlier = polys.slice(0, 10)
-      .filter(p => p.getLength() > 100)
-      .map(p => p.getOutlier(points))
-      .reduce((m, p) => m.d > p.d ? m : p).p;
+    polys = updatePolys(polys, points); // this returns a sorted list of polygons
+    var outlier = polys.slice(0, 10) // only work with the top ten
+      .filter(p => p.getLength() > 100) // make sure they are big enough
+      .map(p => p.getOutlier(points)) // get each ones outlier point
+      .reduce((m, p) => m.d > p.d ? m : p).p; // find the best point and get it
     if (outlier === undefined) break;
     points.push([outlier.x, outlier.y]);
     numTris = polys.length;
     var total = now() - start;
     console.log('num tris', polys.length, 'time', total.toFixed(4));
   }
+  
+  // TODO: Merge like and touching polygons
 
-  polys.forEach(p => p.paint(p.getAvg()));
-
-  for (var i=0; i<data.length; i++) {
-    var pix = data[i];
-    img.set(pix.x, pix.y, 0, pix.r);
-    img.set(pix.x, pix.y, 1, pix.g);
-    img.set(pix.x, pix.y, 2, pix.b);
-  }
+  // PAINT THE POLYGONS
+  polys.forEach(p => {
+    p.paint(p.getAvg());
+    for (var i=0; i<p.length; i++) {
+      var pix = p.data[i];
+      img.set(pix.x, pix.y, 0, pix.r);
+      img.set(pix.x, pix.y, 1, pix.g);
+      img.set(pix.x, pix.y, 2, pix.b);
+    }
+  });
 
   return img;
 }
 
-function updatePolys(polys, points) {
-  var cache = polys.reduce((m, p) => {
-    m[p.id] = p;
-    return m;
-  }, {});
 
-  var delaunay = new Delaunator(points);
-  var triangles = delaunay.triangles;
-  var oldPolys = [];
-  var newPolys = [];
-
-  while (triangles.length > 0) {
-    var edge = triangles.splice(0, 3).map(p => points[p]);
-    var id = edge.join('-');
-    if (cache[id]) {
-      oldPolys.push(cache[id]);
-      delete cache[id];
-    }
-    else {
-      newPolys.push(new Polygon(edge));
-    }
-  }
-
-  var data = Object.keys(cache).reduce((d, k) => {
-    return d.concat(cache[k].data);
-  }, []);
-
-  for (var i=0; i<data.length; i++) {
-    var pix = data[i];
-    var p = [pix.x, pix.y];
-    for (var j=0; j<newPolys.length; j++) {
-      if (newPolys[j].inside(p)) {
-        newPolys[j].add(pix);
-        break;
-      }
-    }
-  }
-
-  var all = oldPolys.concat(newPolys);
-
-  return all.sort(Polygon.sortFn);
-
-}
-
-function makePolys(points, data) {
-  var start = now();
-  var delaunay = new Delaunator(points);
-  var triangles = delaunay.triangles;
-  var polys = [];
-
-  while (triangles.length > 0) {
-    var edge = triangles.splice(0, 3).map(p => points[p]);
-    polys.push(new Polygon(edge));
-  }
-
-  for (var i=0; i<data.length; i++) {
-    var pix = data[i];
-    var p = [pix.x, pix.y];
-    for (var j=0; j<polys.length; j++) {
-      if (polys[j].inside(p)) {
-        polys[j].add(pix);
-        break;
-      }
-    }
-  }
-
-  var total = now() - start;
-
-  console.log('makepoly', 'total', total.toFixed(4), 'num', polys.length, 'per', (total/polys.length).toFixed(4));
-
-  return polys;
-}
